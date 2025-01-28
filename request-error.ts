7bec77da43ff80c5de8e1f68a968615d158031ba
Aurélien Thévoz <aurelien.thevoz@logi.one>
2025-01-28 13:26:16 +0100

    Add generic type to RequestError

--- request-error.ts
+++ request-error.ts
@@ -1,7 +1,7 @@
 import { IncomingMessage } from 'http'
 
-export class RequestError extends Error {
-    constructor(message: string, public readonly status: number, public readonly headers: Headers, public readonly response: Response | IncomingMessage) {
+export class RequestError<T extends Response | IncomingMessage = Response> extends Error {
+    constructor(message: string, public readonly status: number, public readonly headers: Headers, public readonly response: T) {
         super(message)
     }
 }
